b06c40f4a182b249d0d2f1daaae556a2315088c2
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2018-04-20 17:29:25 -0500

    raxielh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-
-
-    </div>
-</div>
+<div style="background: url({{URL::asset('img/bg.jpg')}}) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  height: 100%  !important;
+  min-height: 1200px !important;"></div>
 @endsection
